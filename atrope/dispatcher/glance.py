718d9840c0f4ca0c78bf543125c6999b2ee8ebf5
Enol Fernández <enol.fernandez@egi.eu>
2025-08-14 08:16:12 +0100

    fix: remove vo parameter to _share_image (#17)

--- atrope/dispatcher/glance.py
+++ atrope/dispatcher/glance.py
@@ -291,7 +291,7 @@ class Dispatcher(base.BaseDispatcher):
                             glance_image.id, visibility="shared"
                         )
                     self._share_image(
-                        vo=vo, image=image, glance_image=glance_image, project=project
+                        image=image, glance_image=glance_image, project=project
                     )
 
             self._clean_stale_memberships(glance_image.id, vos, visibility)
